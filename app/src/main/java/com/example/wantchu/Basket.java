543f773453b0bc5865dfe0f82121f9b6f66f7289
kiiiimsj <61073386+kiiiimsj@users.noreply.github.com>
2020-09-04 14:25:06 +0900

    3456789

--- app/src/main/java/com/example/wantchu/Basket.java
+++ app/src/main/java/com/example/wantchu/Basket.java
@@ -102,7 +102,7 @@ public class Basket extends AppCompatActivity implements BootpayRestImplement {
     OrderInsertParsing orderInsertParsing;
     Boolean pay =false;
     private int stuck = 1;
-    private String application_id = "5f3b897b2fa5c20028eecfe8";
+    private String application_id = "5f28e2c002f57e0033305756";
     Long expired_unixtime = null;
     String expired_localtime = null;
     String user_token = null;
