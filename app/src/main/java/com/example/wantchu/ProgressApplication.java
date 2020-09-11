aab44f8b670f79881ae1bd361bc9777e3b0a9244
kiiiimsj <61073386+kiiiimsj@users.noreply.github.com>
2020-09-11 18:29:44 +0900

    1234

---
+++ app/src/main/java/com/example/wantchu/ProgressApplication.java
@@ -0,0 +1,66 @@
+package com.example.wantchu;
+
+import android.app.Activity;
+import android.app.Application;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.text.TextUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatDialog;
+
+public class ProgressApplication extends Application {
+
+    private static ProgressApplication progressApplication;
+    AppCompatDialog progressDialog;
+
+    public static ProgressApplication getInstance(){
+        return progressApplication;
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        progressApplication = this;
+    }
+
+    public void progressON(Activity activity){
+        if(activity == null || activity.isFinishing()){
+            return;
+        }
+
+        if(progressDialog != null && progressDialog.isShowing()){
+            progressSET();
+        }
+        else{
+            progressDialog = new AppCompatDialog(activity);
+            progressDialog.setCancelable(false);
+            progressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+            progressDialog.setContentView(R.layout.progress_loading);
+            progressDialog.show();
+        }
+
+        final ImageView img_loading_frame = (ImageView) progressDialog.findViewById(R.id.frame_loading);
+        final AnimationDrawable frameAnimation = (AnimationDrawable) img_loading_frame.getBackground();
+        img_loading_frame.post(new Runnable() {
+            @Override
+            public void run() {
+                frameAnimation.start();
+            }
+        });
+
+    }
+
+    public void progressSET() {
+
+        if (progressDialog == null || !progressDialog.isShowing()) {
+            return;
+        }
+    }
+    public void progressOFF() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
+}
