90a7449ba08781c9f2585f14a429b07bba3b957c
leeseungjae97 <sky_battle@naver.com>
2020-09-15 22:05:31 +0900

    update StoreList

--- app/src/main/java/com/example/wantchu/MyMap.java
+++ app/src/main/java/com/example/wantchu/MyMap.java
@@ -206,7 +206,7 @@ public class MyMap extends AppCompatActivity implements AutoPermissionsListener,
 //            if(distance >= 1000) {
 //                continue;
 //            }
-            mapListParsing = new MapListParsing(mapListParsing.getStore_name(), mapListParsing.getStore_latitude(), mapListParsing.getStore_longitude(), mapListParsing.getStore_distance());
+            mapListParsing = new MapListParsing(mapListParsing.getStore_name(), mapListParsing.getStore_latitude(), mapListParsing.getStore_longitude(), mapListParsing.getDistance());
             DataList.add(mapListParsing);
         }
         MarkerOptions markerOptions = null;
