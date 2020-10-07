3c9532f939ed0d4e23556ea201715757125b5f6f
leeseungjae97 <sky_battle@naver.com>
2020-10-07 16:44:09 +0900

    udpate web_view

---
+++ app/src/main/java/com/example/wantchu/TermsOfUse.java
@@ -0,0 +1,31 @@
+package com.example.wantchu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class TermsOfUse extends AppCompatActivity {
+    WebView mWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms_of_use);
+
+        mWebView = (WebView) findViewById(R.id.terms_of_use);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.loadUrl("file:///android_asset/www/privacyPolicy.html");
+    }
+}
