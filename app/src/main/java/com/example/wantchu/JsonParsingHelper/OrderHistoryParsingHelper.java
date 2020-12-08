a5163baf074472ac84bf07d30f051c153b277474
hty15973 <61073154+hty15973@users.noreply.github.com>
2020-12-08 19:48:14 +0900

    dafsdfadsfadsf

--- app/src/main/java/com/example/wantchu/JsonParsingHelper/OrderHistoryParsingHelper.java
+++ app/src/main/java/com/example/wantchu/JsonParsingHelper/OrderHistoryParsingHelper.java
@@ -10,18 +10,30 @@ public class OrderHistoryParsingHelper implements Comparable<OrderHistoryParsing
     private int total_price;
     private int total_count;
     private String order_state;
+    private String store_id;
+    private String store_image;
 
     public String getOrder_state() {
         return order_state;
     }
 
-    public OrderHistoryParsingHelper(String order_date, String store_name, String receipt_id, int total_price, int total_count, String order_state) {
+    public OrderHistoryParsingHelper(String order_date, String store_name, String receipt_id, int total_price, int total_count, String order_state,String store_id,String store_image) {
         this.order_date = order_date;
         this.store_name = store_name;
         this.receipt_id = receipt_id;
         this.total_price = total_price;
         this.total_count = total_count;
         this.order_state = order_state;
+        this.store_id = store_id;
+        this.store_image = store_image;
+    }
+
+    public String getStore_image() {
+        return store_image;
+    }
+
+    public void setStore_image(String store_image) {
+        this.store_image = store_image;
     }
 
     public String getOrder_date() {
@@ -44,6 +56,13 @@ public class OrderHistoryParsingHelper implements Comparable<OrderHistoryParsing
         return total_price;
     }
 
+    public String getStore_id() {
+        return store_id;
+    }
+
+    public void setStore_id(String store_id) {
+        this.store_id = store_id;
+    }
 
     @Override
     public int compareTo(OrderHistoryParsingHelper orderHistoryParsingHelper) {
